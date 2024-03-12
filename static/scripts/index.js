6211f9f180de47987bdefc496395350fcf956ec6
Francisco <quicco.sousa@gmail.com>
2024-03-12 21:57:44 +0000

    Changed the Pokédex display to Cards. Added a Go Back btn. Styled individual PkMn Pages

--- static/scripts/index.js
+++ static/scripts/index.js
@@ -1,8 +1,25 @@
-const rows = document.querySelectorAll('.data');
+const cards = document.querySelectorAll('.card-pokedex');
+
 // Add an eventListener to every row
-for (let i = 0; i < rows.length; i++) {
-  rows[i].addEventListener('click', () => {
-    const form = rows[i].querySelector('.pkmn-data');
+for (const card of cards) {
+  card.addEventListener('click', () => {
+    const form = card.querySelector('.pkmn-data');
     form.submit();
-  })
+  });
+}
+// Add an eventListener to every row
+// for (let i = 0; i < rows.length; i++) {
+//   rows[i].addEventListener('click', () => {
+//     const form = rows[i].querySelector('.pkmn-data');
+//     form.submit();
+//   })
+// }
+
+// Add 'Go Back' functionality to a button
+const goBackBtn = document.querySelector('.go-back');
+
+if (goBackBtn) {
+  goBackBtn.addEventListener('click', () => {
+    window.history.back();
+  });
 }
